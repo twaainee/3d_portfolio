5ba4398aa340a002e20a53eb08828850bfc7fd63
Elijah Twaine Marquez <twainemarquez@gmail.com>
2026-02-26 00:05:08 +0800

    Add id attribute to Hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -11,7 +11,7 @@ import Loader from '../components/Loader'
 const Hero = () => { 
   const isMobile = useMediaQuery({maxWidth: 853})
   return (
-    <section className="flex items-start justify-center md:items-start md:justify-start min-h-screen overflow-hidden c-space">
+    <section id="home" className="flex items-start justify-center md:items-start md:justify-start min-h-screen overflow-hidden c-space">
         <HeroText />
         <ParallaxBackground />
         <figure className="absolute inset-0" style={{width: "100vw", height: "100vh"}}>
