2dee7a5a5e0ff6309feb757c1ce5cef5659bc609
Elijah Twaine Marquez <twainemarquez@gmail.com>
2026-02-26 00:05:29 +0800

    Update About section with new ID and structure

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -6,9 +6,9 @@ import { Frameworks } from "../components/Frameworks"
 
 const About = () => {
     const grid2Container = useRef()
-  return (
-    <section className="c-space section-spacing">
-        <h2 className="text-heading">About Me</h2>
+    return (
+        <section id="about" className="c-space section-spacing">
+                <h2 className="text-heading">About Me</h2>
         <div className="grid grid-cols-1 gap-4 md:grid-cols-6 md:auto-rows-[18rem] mt-12">
             {/* Grid 1 */}
             <div className="flex items-end grid-default-color grid-1">
