493187fc0308a3a4b40d55c8a505e2bc4fd38c94
Elijah Twaine Marquez <twainemarquez@gmail.com>
2026-06-23 21:47:02 +0800

    changed flipwords

--- src/components/HeroText.jsx
+++ src/components/HeroText.jsx
@@ -5,7 +5,7 @@ import { motion } from "motion/react"
 
 
 const HeroText = () => {
-const Words = [ "Modern", "Scalable", "Minimalist", "Aesthetic"];
+const Words = [ "Modern", "Scalable", "Aesthetic"];
 const Variants = {
     hidden:{opacity: 0, x: -50},
     visible:{opacity: 1, x: 0},
